c7550f6f4aa2d5f9c62350a29d1372226c56cf8e
悦兮 <1031616423@qq.com>
2020-03-26 10:03:03 +0900

    Site updated: 2020-03-26 10:02:59

---
+++ js/aplayer-el.js
@@ -0,0 +1,91 @@
+class APlayerElement extends HTMLElement {
+
+  connectedCallback() {
+    if (window.APlayer && window.fetch) {
+      this._init();
+    }
+  }
+
+  disconnectedCallback() {
+    if (!this.lock && this.aplayer) {
+      this.aplayer.list.clear();
+      this.aplayer.destroy();
+    }
+  } 
+
+  _camelize(str) {
+    return str
+      .replace(/^[_.\- ]+/, '')
+      .toLowerCase()
+      .replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase());
+  }
+
+  _init() {
+    let config = {};
+    // 复制配置
+    for (let i = 0; i < this.attributes.length; i += 1) {
+      config[this._camelize(this.attributes[i].name)] = this.attributes[i].value;
+    }
+    // 设置容器
+    if (!config.container) {
+      let div = document.createElement('div');
+      config.container = div;
+      this.appendChild(div);
+    } else {
+      let div = document.getElementById(config.container);
+      config.container = div;
+    }
+    this._loadAudios(config);
+    this._convertType(config);
+
+    this.aplayer = new APlayer(config);
+  }
+
+  _convertType(config) {
+    for (let key in config) {
+      if (config[key] === 'true' || config[key] === 'false') {
+        config[key] = (config[key] === 'true')
+      }
+    }
+    if (config.lrcType) {
+      config.lrcType = parseInt(config.lrcType);
+    }
+  }
+
+  _loadAudios(config) {
+    if (!config.audio) {
+      return;
+    }
+    let folder = '';
+    if (config.folder) {
+      folder = config.folder;
+      delete config.folder;
+    }
+    let parseOrder = ["artist", "name", "cover", "lrc", "theme"];
+    let audio = config.audio.split(';');
+    let _audio = [];
+    audio.forEach((single) => {
+      single = single.split(',');
+      let s = {};
+      for (let i = 0; i < single.length; i++) {
+        s[parseOrder[i]] = single[i].trim();
+      }
+      s.name = s.name || '';
+      s.artist = s.artist || '';
+      s.url = folder + s.artist + ' - ' + s.name + '.mp3';
+      if (s.cover === '-') {
+        s.cover = folder + 'cover/' + s.artist + ' - ' + s.name + '.jpg';
+      }
+      if (s.lrc === '-') {
+        s.lrc = folder + 'lyric/' + s.artist + ' - ' + s.name + '.lrc';
+      }
+      _audio.push(s);
+    });
+    config.audio = _audio;
+  }
+}
+
+if (window.customElements && !window.customElements.get('aplayer-js')) {
+  window.APlayerElement = APlayerElement;
+  window.customElements.define('aplayer-js', APlayerElement);
+}
---
+++ js/videojs-bilibili-el.js
@@ -0,0 +1,102 @@
+class VideoJSBilibiliEl extends HTMLElement {
+
+  connectedCallback() {
+    if (window.videojs && window.fetch) {
+      this.videojs_el = document.createElement('video-js');
+      this.videojs_el.classList.add('videojs-bilibili');
+      this.appendChild(this.videojs_el);
+
+      this.playlist_el = document.createElement('div');
+      this.playlist_el.classList.add('vjs-playlist-wrap');
+      this.appendChild(this.playlist_el);
+
+      this._init();
+    }
+  }
+
+  disconnectedCallback() {
+    if (!this.lock && this.vplayer) {
+      this.vplayer.dispose();
+      delete this.vplayer;
+    }
+  }
+
+  _camelize(str) {
+    return str
+      .replace(/^[_.\- ]+/, '')
+      .toLowerCase()
+      .replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase());
+  }
+
+  _init() {
+    let config = {};
+    // 复制配置
+    for (let i = 0; i < this.attributes.length; i += 1) {
+      config[this._camelize(this.attributes[i].name)] = this.attributes[i].value;
+    }
+    this._initVPlayer();
+    this._loadVideos(config);
+    this.vplayer.playlistUi();
+  }
+
+  _initVPlayer() {
+    let defaultOptions = {
+      autoplay: false,
+      controls: false,
+      loop: false,
+      muted: false,
+      preload: 'auto',
+      // poster: '',
+      // sources: {},
+      aspectRatio: '16:9',
+      fluid: true,
+      inactivityTimeout: 2000,
+      liveui: false,
+      fullscreen: { navigationUI: 'hide' },
+      playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 2],
+      plugins: {},
+      children: {
+        LoadingSpinner: {},
+        BiliBigPlayButton: {},
+        BiliTop: {},
+        BiliControlBar: {}
+      }
+    };
+    this.vplayer = videojs(this.videojs_el, defaultOptions, function() {
+      this.on(this.tech_, 'click', 
+        videojs.getComponent('BiliPlayToggle').prototype.handleClick);
+    });
+  }
+
+  _loadVideos(config) {
+    if (!config.video) {
+      return;
+    }
+    let folder = config.folder || '';
+    let _video = config.video.split(';');
+    let video = [];
+    _video.forEach((i) => {
+      let item = i.split(',');
+      for (let i = 0; i < item.length; i++) {
+        item[i] = item[i].trim();
+      }
+
+      let meta = {
+        author: item[0],
+        title: item[1],
+        sources: [{
+          src: folder + item[0] + " - " + item[1] + ".mp4",
+          type: "video/mp4"
+        }],
+        duration: item[2]
+      }
+      video.push(meta);
+    });
+    this.vplayer.playlist(video);
+  }
+}
+
+if (window.customElements && !window.customElements.get('videojs-bilibili')) {
+  window.VideoJSBilibiliEl = VideoJSBilibiliEl;
+  window.customElements.define('videojs-bilibili', VideoJSBilibiliEl);
+}
